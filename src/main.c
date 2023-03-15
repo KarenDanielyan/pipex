418ee2961261d65995e25d941c17c0f74eb2f890
KarenDanielyan <karendanielyan66@gmail.com>
2023-03-15 19:53:22 +0400

    Starting all over again

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 18:11:53 by kdaniely          #+#    #+#             */
-/*   Updated: 2023/03/14 22:34:05 by kdaniely         ###   ########.fr       */
+/*   Updated: 2023/03/15 19:53:07 by kdaniely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,34 +16,12 @@
 #include "parse.h"
 #include "pipex.h"
 
-/*static void	check_input(int ac, char **av)
-{
-	if (ac < 5 && !BONUS)
-	{
-		ft_printf("Insufficient number of arguments.\n");
-		exit(EXIT_FAILURE);
-	}
-	if (get_arg_type(av[0]) == FIL && get_arg_type(av[ac - 1]) == FIL)
-	{
-		if (access(av[0], F_OK) == -1 || access(av[ac - 1], F_OK) == -1)
-		{
-			perror("Access: ");
-			exit(EXIT_FAILURE);
-		}
-	}
-	else
-	{
-		ft_printf("First and last arguments are not files!!!\n");
-		exit(EXIT_FAILURE);
-	}
-}*/
-
 static void	redirect_io(int ac, char **av)
 {
 	int	fd[2];
 	
-	fd[0] = open(av[1], O_RDONLY);
-	fd[1] = open(av[ac - 1], O_WRONLY);
+	fd[0] = open(av[1], O_RDONLY | O_CREAT, 0700);
+	fd[1] = open(av[ac - 1], O_WRONLY | O_CREAT, 0700);
 	if (fd[0] == -1 || fd[1] == -1)
 	{
 		perror("Open: ");
@@ -51,60 +29,31 @@ static void	redirect_io(int ac, char **av)
 	}
 	dup2(fd[0], STDIN_FILENO);
 	dup2(fd[1], STDOUT_FILENO);
-}
-
-static void	execute_pipe(char **cmd, char *path, char **envp)
-{
-	int		pid;
-	t_pipe	pipe;
-
-	pipe = get_pipe();
-	pid = fork();
-	if (pid)
-	{
-		close(pipe.out);
-		dup2(pipe.in, STDIN_FILENO);
-		wait(NULL);
-	}
-	else
-	{
-		close(pipe.in);
-		dup2(pipe.out, STDOUT_FILENO);
-		if (execve(path, cmd, envp) == -1)
-		{
-			perror("execve: ");
-			exit(EXIT_FAILURE);
-		}
-		else
-			exit(EXIT_SUCCESS);
-	}
-}
-
-static void	loop(int ac, char **av, char **envp, char **path)
-{
-	int	i;
-	char	**cmd;
-	char	*bin_path;
-
-	i = 2;
-	while (i < (ac - 1))
-	{
-		cmd = ft_split(*(av + i), ' ');
-		bin_path = get_file_path(path, *cmd);
-		execute_pipe(cmd, bin_path, envp);
-		i ++;
-		free(bin_path);
-		free(cmd);		//TODO: Add 2d split
-	}
+	close(fd[0]);
+	close(fd[1]);
 }
 
 int	main(int ac, char **av, char **envp)
 {
 	char	**path;
-
+	t_pipe	pipe;
+	int		pid;
+	
 	path = get_path(envp);
 	redirect_io(ac, av);
-	execute_pipe(ft_split("wc -l", ' '), "/usr/bin/wc", envp);
-	//loop(ac, av, envp, path);
+	
+	pipe = get_pipe();
+	pid = fork();
+	if (pid)
+	{
+		close(pipe.out);
+		close(pipe.in);
+		wait(NULL);
+	}
+	else
+	{
+		char	**cmd = ft_split(av[2], ' ');
+		execve(get_file_path(path, cmd[0]), cmd, envp);
+	}
 	return (0);
 }
--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 13:44:29 by kdaniely          #+#    #+#             */
-/*   Updated: 2023/03/14 21:53:49 by kdaniely         ###   ########.fr       */
+/*   Updated: 2023/03/15 19:47:32 by kdaniely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,48 +61,4 @@ char	**get_path(char **envp)
 	split = ft_split(path, ':');
 	free(path);
 	return (split);
-}
-
-void	fork_in_main(t_list **pid_l)
-{
-	t_list	*head;
-	int	*n;
-
-	head = *pid_l;
-	if (head)
-	{
-		if (ft_lstsize(head) >= 1 && *((int *)(head->content)) != 0)
-		{
-			n = malloc(sizeof(int));
-			*n = fork();
-			ft_lstadd_back(pid_l, ft_lstnew(n));
-		}
-	}
-	else
-	{
-		n = malloc(sizeof(int));
-		*n = fork();
-		ft_lstadd_back(pid_l, ft_lstnew(n));
-	}
-}
-
-char	*read_file(int	fd)
-{
-	char	*content;
-	char	*tmp;
-	
-	tmp = get_next_line(fd);
-	content = NULL;
-	while (tmp)
-	{
-		if (content)
-		{
-			content = ft_strjoin(content, tmp);
-			free(tmp);
-		}
-		else
-			content = tmp;
-		tmp = get_next_line(fd);
-	}
-	return (content);
 }
