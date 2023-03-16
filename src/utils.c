65eb5c41d860c96c78eedac32c185d42350e3bdc
KarenDanielyan <karendanielyan66@gmail.com>
2023-03-16 21:02:43 +0400

    HOTFIX

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 18:41:25 by kdaniely          #+#    #+#             */
-/*   Updated: 2023/03/16 20:09:27 by kdaniely         ###   ########.fr       */
+/*   Updated: 2023/03/16 21:01:19 by kdaniely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,11 @@ char	*get_file_path(char **path, char *file)
 	char	*join;
 
 	if (access(file, X_OK) == 0)
-		return (file);
-	file_w_slash = ft_strjoin("/", file);
+		return (ft_strdup(file));
+	if (file[0] != '/')
+		file_w_slash = ft_strjoin("/", file);
+	else
+		file_w_slash = file;
 	while (path)
 	{
 		join = ft_strjoin(*path, file_w_slash);
@@ -32,7 +35,8 @@ char	*get_file_path(char **path, char *file)
 		free(join);
 		join = NULL;
 	}
-	free(file_w_slash);
+	if (file[0] != '/')
+		free(file_w_slash);
 	return (join);
 }
 
